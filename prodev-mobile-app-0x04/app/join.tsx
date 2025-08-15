2c89fdae5a40c8424591bc02403ca170b0888f26
kamau552 <faithkamau730@gmail.com>
2025-08-15 16:48:38 +0300

    Navigation using Stack Navigation Patterns for mobile development with react native

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,74 @@
+import { Text, TextInput, View, TouchableOpacity, Image } from "react-native";
+import { styles } from "@/styles/_joinstyle";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import { FontAwesome } from "@expo/vector-icons";
+import { HEROLOGOGREEN, GOOGLELOGO, FACEBOOKLOGO } from "@/constants";
+
+export default function Join() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.iconsection}>
+          <FontAwesome name="arrow-left" size={24} color="black" />
+          <Image source={HEROLOGOGREEN} />
+          <View style={{ width: 24 }} /> {/* Spacer for alignment */}
+        </View>
+
+        <View style={styles.titleTextGroup}>
+          <Text style={styles.titleText}>Create your</Text>
+          <Text style={styles.titleText}>Account</Text>
+          <Text style={styles.subText}>Fill in your details to continue</Text>
+        </View>
+
+        <View style={styles.formGroup}>
+          <View>
+            <Text style={styles.formLabel}>Full Name</Text>
+            <TextInput style={styles.formControl} />
+          </View>
+          
+          <View>
+            <Text style={styles.formLabel}>Email</Text>
+            <TextInput keyboardType="email-address" style={styles.formControl} />
+          </View>
+          
+          <View>
+            <Text style={styles.formLabel}>Password</Text>
+            <View style={styles.formPasswordControl}>
+              <TextInput secureTextEntry style={styles.passwordControl} />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>
+          </View>
+          
+          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+        </View>
+
+        <TouchableOpacity style={styles.primaryButton}>
+          <Text style={styles.buttonText}>Sign up</Text>
+        </TouchableOpacity>
+
+        <View style={styles.dividerGroup}>
+          <View style={styles.divider}></View>
+          <Text style={styles.dividerText}>OR</Text>
+          <View style={styles.divider}></View>
+        </View>
+
+        <View style={styles.secondaryButton}>
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Image source={GOOGLELOGO} />
+            <Text style={styles.secondaryButtonText}>Continue with Google</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.secondaryButton}>
+            <Image source={FACEBOOKLOGO} />
+            <Text style={styles.secondaryButtonText}>Continue with Facebook</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.signupgroup}>
+          <Text style={styles.signupTitleText}>Already have an account?</Text>
+          <Text style={styles.signupSubTitleText}>Sign in</Text>
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
